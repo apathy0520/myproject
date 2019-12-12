3dc98711222ed12fbf1bbee6146f8b774a459ee6
Apathy <58775481+apathy0520@users.noreply.github.com>
2019-12-12 16:58:25 +0800

    golang练习

---
+++ golangTest/hello.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+    "fmt"
+)
+
+func main(){
+	const (
+		limit = iota
+		top  = iota
+	)
+	const Pi  = iota  
+	const x,y int = 1,3
+
+	var a int
+	var b string
+	var c float64
+	var d [5] int
+	var e [] int
+	var f * int
+	var v1 int = 5
+	var  v2 = 3
+	v3 := 5
+	v1,v2 = v2,v1
+
+    fmt.Println("Hello,World!Are you OK?")
+    fmt.Println(limit,top,Pi,x,y)
+    fmt.Println(a,b,c,d,e,f,v1,v2,v3)
+}
